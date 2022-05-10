cc3da29a503dd186f2d187fb3447cb3237ed005d
bavly74 <bavly.eskander74@gmail.com>
2022-05-10 18:40:09 +0200

    modify

--- men.php
+++ men.php
@@ -186,7 +186,7 @@ while($row=$itemRes->fetch_assoc()){
                     <div class="col-lg-4 ">
                         <div class="card d-flex flex-column align-items-center">
                             <div class="product-name"><?php echo $row["name"]; ?></div>
-                            <div class="card-img"> <img src="images/<?php echo $row["imgPath"]; ?>" alt=""> </div>
+                            <div class="card-img"> <img src="img/<?php echo $row["imgPath"]; ?>" alt=""> </div>
                             <div class="card-body pt-5">
                                 <div class="d-flex align-items-center price">
                                     <div class="font-weight-bold"><?php echo $row["price"]; ?>$</div>
