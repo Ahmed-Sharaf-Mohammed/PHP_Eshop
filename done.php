f743d6c11fad3073c824f773497a8f73b99706ac
bavly74 <bavly.eskander74@gmail.com>
2022-05-14 01:39:01 +0200

    modify done page

--- done.php
+++ done.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="css/done.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+
 </head>
 <body>
-<h1>Done</h1>
-<script src="js/bootstrap.min.js"></script>
+    <div class="done">
+    <div class="doneBox">
+        <h1>Done</h1>
+        <div class="doneSign"><i class="far fa-check-circle"></i></div>
+        <div class="backToHome"><a href="home.php">back to home</a></div>
+</div>
+
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.js"></script>
+        <script src="css/fontawesome-free-5.15.4-web/js/all.js"></script>
+        <script src="js/js.js"></script>
 </body>
 </html>
