9cd44160fe3628db889edad1f45ae7d813b239ff
bavly74 <bavly.eskander74@gmail.com>
2022-05-17 18:57:57 +0200

    modify home hrader

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 
     <div id="header-3">
     <div class="estore-logo"><img src="img/download (2).webp"></div>
-<div id="header-3-links">
+  <div id="header-3-links">
     <ul>
         <li><a href="home.php">Home</a></li>
         <li><a href="#">About Us</a></li>
